a24a086f1f15b1e017550604fd4eec0631fedb03
Emilse Arias <emilsearias@gmail.com>
2024-02-14 10:27:49 -0300

    agregamos las rutas del proyectos en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import Footer from "./components/common/Footer";
 import Menu from "./components/common/Menu";
 import Error404 from "./components/pages/Error404";
 import Administrador from "./components/pages/Administrador";
 import Inicio from "./components/pages/Inicio";
-import FormularioProducto from './components/pages/producto/FormularioProducto';
+import FormularioProducto from "./components/pages/producto/FormularioProducto";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Menu></Menu>
-      {/* <Inicio></Inicio> */}
-      {/* <Administrador></Administrador> */}
- 
-      <Error404></Error404>
+      <Routes>
+        <Route exact path="/" element={<Inicio></Inicio>}></Route>
+        <Route exact path="/administrador"
+          element={<Administrador></Administrador>}
+        ></Route>
+        <Route path="*" element={<Error404></Error404>}></Route>
+      </Routes>
       <Footer></Footer>
-    </>
+    </BrowserRouter>
   );
 }
 
