aeb768420af49d5c48b1aa705327fc59534cfaca
Emilse Arias <emilsearias@gmail.com>
2024-02-19 09:49:05 -0300

    agregamos los iconos al componente Administrador

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap-icons/font/bootstrap-icons.min.css';
 import "./App.css";
 import Footer from "./components/common/Footer";
 import Menu from "./components/common/Menu";
